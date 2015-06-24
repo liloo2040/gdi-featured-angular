7a9f60f389a862efa7730625e015b05615397cec
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2015-06-24 19:05:15 -0400

    add more steps to controller exercise

--- exercises/recipe-app/app.js
+++ exercises/recipe-app/app.js
@@ -1,5 +1,21 @@
 (function(){
 
+    angular
+        .module('myApp', [])
 
+        .controller('RecipeController', function(){
+            this.recipe = recipe
+        });
+
+    var recipe = {
+        name: 'Waffles',
+        cookTime: 9,
+        prepTime: 10,
+        ingredients: ['flour', 'water', 'sugar', 'vanilla'],
+        directions: "Mix everything together in a bowl with a whisk. Use a waffle press to make your waffles!",
+        isVegan: false,
+        isPaleo: true,
+        isGlutenFree: true
+    };
 
 })();
