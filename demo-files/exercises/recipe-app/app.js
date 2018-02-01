9ab1bb4f527b0ae26c908e8d827c414befe2de98
liloo2040 <e.dufour.89@gmail.com>
2018-02-01 23:16:57 +0100

    adding object

--- demo-files/exercises/recipe-app/app.js
+++ demo-files/exercises/recipe-app/app.js
@@ -1,9 +1,17 @@
 (function(){
 angular
     .module('recipeApp', [])
-    .controller('', function(){
-
+    .controller('RecipeController', function(){
+        this.recipe = recipe;
     });
-
+var recipe = {
+    name:,
+    ingredients: ['flour', 'eggs', 'chocolate','butter'],
+    prepTime:,
+    cookTime:,
+    isVegan:,
+    isGlutenFree:,
+    isPaleo:
+};
 
 })();
